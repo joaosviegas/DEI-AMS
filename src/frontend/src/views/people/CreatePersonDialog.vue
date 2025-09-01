716c35aaeef631273ecd905e057bb5f2130d033c
João Sérgio Viegas <joao.s.viegas@tecnico.ulisboa.pt>
2025-09-01 15:04:34 +0100

    chore: button consistency

--- src/frontend/src/views/people/CreatePersonDialog.vue
+++ src/frontend/src/views/people/CreatePersonDialog.vue
@@ -33,7 +33,7 @@
         <v-card-actions>
           <v-spacer></v-spacer>
 
-          <v-btn text="Sair" variant="plain" @click="dialog = false"></v-btn>
+          <v-btn text="Cancelar" variant="plain" @click="dialog = false"></v-btn>
 
           <v-btn
             color="primary"
